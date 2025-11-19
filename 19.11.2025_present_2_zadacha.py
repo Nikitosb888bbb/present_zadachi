f305780f75d40441075e83cb58f009418c273a41
Nikitosb888bbb <nikitakuzya2007@gmail.com>
2025-11-19 11:55:19 +0300

    19.11.2025_present_2_zadacha.py

--- 19.11.2025_present_2_zadacha.py
+++ 19.11.2025_present_2_zadacha.py
@@ -1,9 +1,12 @@
 side_a=float(input('Введите длину стороны А: '))
 side_b=float(input('Введите длину стороны B: '))
 side_c=float(input('Введите длину стороны C: '))
-if side_a==side_b==side_c:
-    print('Треугольник равносторонний')
-elif side_a==side_b or side_a==side_c or side_b==side_c:
-    print('Треугольник равнобедренный')
+if a+c>b and a+b>c and b+c>a:
+    if side_a==side_b==side_c:
+        print('Треугольник равносторонний')
+    elif side_a==side_b or side_a==side_c or side_b==side_c:
+        print('Треугольник равнобедренный')
+    else:
+        print('Треугольник разносторонний')
 else:
-    print('Треугольник разносторонний')
+    print('Треугольника не существует')
