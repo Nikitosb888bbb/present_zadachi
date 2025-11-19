2675ff95d3580d899c921708f9b49ebc6c7dee2e
Nikitosb888bbb <nikitakuzya2007@gmail.com>
2025-11-19 11:42:23 +0300

    Add files via upload

---
+++ 19.11.2025_present_2_zadacha.py
@@ -0,0 +1,9 @@
+side_a=float(input('Введите длину стороны А: '))
+side_b=float(input('Введите длину стороны B: '))
+side_c=float(input('Введите длину стороны C: '))
+if side_a==side_b==side_c:
+    print('Треугольник равносторонний')
+elif side_a==side_b or side_a==side_c or side_b==side_c:
+    print('Треугольник равнобедренный')
+else:
+    print('Треугольник разносторонний')
